607d11357f94c22babdb9273ee263da081507da3
JeanpierJY <jeanpierre827xd@gmail.com>
2026-05-14 23:41:27 -0500

    Update SistemaHospitalario.java

--- src/sistema/hospitalario/SistemaHospitalario.java
+++ src/sistema/hospitalario/SistemaHospitalario.java
@@ -5,7 +5,8 @@ public class SistemaHospitalario {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
-        
+        //Añadi este mensaje
+        System.out.println("Jeremy es gay");
         int x = 12;
     }
     
